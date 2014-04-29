c84d956a67a56d87366ef72e251f422e6110f255
David Pany <DFIRDave@gmail.com>
2014-04-28 22:42:16 -0400

    First Version
    This is version 1.0 of Execution Protector.bat

---
+++ Execution Protector.bat
@@ -0,0 +1,13 @@
+::Written by @DavidPany
+::This batch file adds a "_" to the extension of 
+::every file in the current directory (except itself)
+
+::version 1.0
+
+ECHO OFF
+for %%f in (*.*) do (
+	IF NOT "%~n0%~x0" == "%%f" (
+		ren "%%f" "%%f_"
+		)
+	)
+PAUSE
